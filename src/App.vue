4ff22abd1de71beec4b65555e55e83214f45c227
Jason Crawford <daxapps777@gmail.com>
2017-12-05 21:00:28 -0600

    Display panel after Submit btn clicked

--- src/App.vue
+++ src/App.vue
@@ -60,13 +60,13 @@
       <hr>
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-          <button class="btn btn-primary">Submit!
+          <button class="btn btn-primary" @click.prevent="submitted">Submit!
           </button>
         </div>
       </div>
     </form>
     <hr>
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -83,7 +83,6 @@
             </ul>
             <p>Gender: {{ gender }}</p>
             <p>Priority: {{ selectedPriority }}</p>
-            <p>Switched:</p>
           </div>
         </div>
       </div>
@@ -103,7 +102,13 @@ export default {
       sendMail: [],
       gender: 'Male',
       selectedPriority: 'High',
-      priorities: ['High', 'Medium', 'Low']
+      priorities: ['High', 'Medium', 'Low'],
+      isSubmitted: false
+    }
+  },
+  methods: {
+    submitted () {
+      this.isSubmitted = true
     }
   }
 }
